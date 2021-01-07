f90ce2105552289d90eb7d742ccdf53a13669f0c
EvgeniySevostyanihin <sevostyanikhin02@gmail.com>
2021-01-08 01:47:40 +0300

    few changes

--- dirt_tongue/__init__.py
+++ dirt_tongue/__init__.py
@@ -1,4 +1,4 @@
-from dirt_tongue import *
+from dirt_tongue.dirt_tongue import is_dirt, standart_dirt
 
 
 __version__ = "0.1"
--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from distutils.core import setup, find_packages
 import dirt_tongue
+import os 
 
 
 setup(
     name = "dirt_tongue",
     version = dirt_tongue.__version__,
+    author = "Evgeniy Sevostyanihin",
+    description = "search russian dirty word",
+    long_description = open(os.join(dirname(__file__), 'README.md')).read(),
     packages = find_packages(),
     url = "https://github.com/BabylenMagnus/dirt_tongue",
     author_email='sevostyanikhin02@gmail.com',
-    license="GPL3"
+    license="GPL3",
 )
-
