ef95dc5ba907d2b99f8bbc6d37bf5a20e6b2f038
EvgeniySevostyanihin <sevostyanikhin02@gmail.com>
2021-01-08 10:33:14 +0300

    bug in setup

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ from distutils.core import setup
 import dirt_tongue
 
 
+with open("README.md", "r", encoding='utf-8') as t:
+    long_description = t.read()
+
+
 setup(
     name = "dirt_tongue",
     version = dirt_tongue.__version__,
     author = "Evgeniy Sevostyanihin",
     description = "search russian dirty word",
-    long_description = open('README.md').read(),
+    long_description = long_description,
     packages = ['dirt_tongue'],
     url = "https://github.com/BabylenMagnus/dirt_tongue",
     author_email='sevostyanikhin02@gmail.com',
